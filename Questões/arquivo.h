340b9e39b11f1c5cd5e355a5ecfef200f6744153
Gabriel-Marti <gabriel.marti@dcomp.ufs.br>
2026-02-10 00:08:31 +0000

    Add files via upload

---
+++ Questões/arquivo.h
@@ -0,0 +1,12 @@
+
+#ifndef ARQUIVO_H 
+#define ARQUIVO_H
+
+#include <stdio.h> // Importante para ponteiros de arquivos (FILE*) ser reconhecido aqui
+
+//cabeçalho das funções
+void gestao_q1(FILE* arq, FILE* bios);
+void gestao_q2(FILE* arq);
+void gestao_q3(FILE* arq, FILE* bios);
+void gestao_q4(FILE* arq, FILE* bios);
+#endif
